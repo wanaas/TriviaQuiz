11f8b1eba3d4d443a665878cc3a0bad9e043d987
Trenton Moore <tamoore71@gmail.com>
2019-08-24 14:55:01 -0700

    added questions array, question prompt

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,15 @@
+var questions = [
+    [1, "Edward in the Twilight series is a 'vegetarian vampire', meaning he drinks only non-human, animal blood.", "true"],
+    [2, "The African elephant is the largest carnivore on land.", "false"],
+    [3, "The deadliest weapon of World War I was the machine gun.", "true"],
+    [4, "In the famous novel of the same name, Frankenstein is a monster constructed from body parts.", "false"],
+    [5, "Dracula was a real-life figure in history.", "false"],
+    [6, "Tug-of-war was once an Olympic sport", "true"]
+];
+
+var answer;
+var response;
+
+answer = prompt(questions[0][1]);
+
+console.log(questions);
